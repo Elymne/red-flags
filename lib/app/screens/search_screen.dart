8d40ee2601ae5ba642144014a4fff0617f5c75bb
Elymne <sacha.djurdjevic@gmail.com>
2025-04-28 16:44:23 +0200

    Inprogress : adding desc text

--- lib/app/screens/search_screen.dart
+++ lib/app/screens/search_screen.dart
@@ -6,9 +6,10 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:red_flags/app/widgets/fantom_widget.dart';
 import 'package:red_flags/app/widgets/shakle_input.dart';
 import 'package:red_flags/app/widgets/shakle_outlined_button.dart';
+import 'package:red_flags/app/widgets/shakle_text.dart';
 import 'package:red_flags/app/widgets/title_container.dart';
-import 'package:red_flags/models/person.model.dart';
 import 'package:red_flags/providers/persons/search_persons.provider.dart';
+import 'package:red_flags/providers/provider_value.dart';
 
 class SearchScreen extends ConsumerStatefulWidget {
   const SearchScreen({super.key});
@@ -38,6 +39,7 @@ class _State extends ConsumerState<SearchScreen> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     final persons = ref.watch(searchPersonsProvider).value;
+    final indexResult = _getIndexResult();
 
     return Scaffold(
       body: Column(
@@ -113,52 +115,66 @@ class _State extends ConsumerState<SearchScreen> with TickerProviderStateMixin {
               ),
             ),
           ),
-          // Validation Button with result number.
-          FantomWidget(
-            duration: Duration(milliseconds: 2000),
-            child: SizedBox(
-              height: 100,
-              child: PageView(
-                controller: _pageController,
-                physics: NeverScrollableScrollPhysics(),
-                children: [
-                  Align(
-                    alignment: Alignment.center,
-                    child: ShakleOutlinedButton(
-                      AppLocalizations.of(context)!.searchButton,
-                      animColor: Theme.of(context).colorScheme.primary,
-                      isActive: false,
-                      onPressed: () {
-                        if (kDebugMode) print("Clicked");
-                      },
-                    ),
-                  ),
 
-                  Align(
-                    alignment: Alignment.center,
-                    child: ShakleOutlinedButton(
-                      "${AppLocalizations.of(context)!.searchButton} (${persons.length})",
-                      animColor: Theme.of(context).colorScheme.primary,
-                      isActive: true,
-                      onPressed: () {
-                        if (kDebugMode) print("Clicked");
-                      },
-                    ),
-                  ),
-
-                  Align(
-                    alignment: Alignment.center,
-                    child: ShakleOutlinedButton(
-                      AppLocalizations.of(context)!.createButton,
-                      animColor: Theme.of(context).colorScheme.primary,
-                      isActive: true,
-                      onPressed: () {
-                        if (kDebugMode) print("Clicked");
-                      },
-                    ),
-                  ),
-                ],
+          // TODO : Message helper (error, advice, guidance).
+          SizedBox(height: 20),
+          if (indexResult == 0)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: ShakleText(
+                "Il faut chercher, allez hop hop hop!",
+                style: Theme.of(context).textTheme.bodyLarge,
+                animColor: Theme.of(context).colorScheme.primary,
+                hasIdleAnim: true,
               ),
+            ),
+
+          // Validation Button with result number.
+          SizedBox(
+            height: 100,
+            child: PageView(
+              controller: _pageController,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                // Default value.
+                SizedBox(),
+                // Not found.
+                Align(
+                  alignment: Alignment.center,
+                  child: ShakleOutlinedButton(
+                    AppLocalizations.of(context)!.noneButton,
+                    animColor: Theme.of(context).colorScheme.primary,
+                    isActive: false,
+                    onPressed: () {
+                      if (kDebugMode) print("Clicked");
+                    },
+                  ),
+                ),
+                // Found.
+                Align(
+                  alignment: Alignment.center,
+                  child: ShakleOutlinedButton(
+                    "${AppLocalizations.of(context)!.lookButton} (${persons.length})",
+                    animColor: Theme.of(context).colorScheme.primary,
+                    isActive: true,
+                    onPressed: () {
+                      if (kDebugMode) print("Clicked");
+                    },
+                  ),
+                ),
+                // Not found and and you can create a new one.
+                Align(
+                  alignment: Alignment.center,
+                  child: ShakleOutlinedButton(
+                    AppLocalizations.of(context)!.createButton,
+                    animColor: Theme.of(context).colorScheme.primary,
+                    isActive: true,
+                    onPressed: () {
+                      if (kDebugMode) print("Clicked");
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ],
@@ -176,26 +192,40 @@ class _State extends ConsumerState<SearchScreen> with TickerProviderStateMixin {
       await ref
           .read(searchPersonsProvider.notifier)
           .search(firstname: _firstname, lastname: _lastname, zoneName: _zonename, jobname: _jobname);
-      // Get persons from search ahead.
-      final persons = ref.read(searchPersonsProvider).value;
-      // Switch to ListView Screen Button.
-      if (persons.isNotEmpty) {
-        _pageController.animateToPage(1, duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+
+      // Display the correct button depending of result from provider (found, not found, error).
+      final index = _getIndexResult();
+      if (index > 3) {
+        // An index higher than 3 mean an error occured.
+        _pageController.animateToPage(0, duration: Duration(milliseconds: 400), curve: Curves.easeIn);
         return;
       }
-      // Switch to CreateNew Screen Button.
-      if (_firstname.isNotEmpty &&
-          _lastname.isNotEmpty &&
-          _zonename.isNotEmpty &&
-          _jobname.isNotEmpty &&
-          persons.isNotEmpty &&
-          persons.isEmpty) {
-        _pageController.animateToPage(2, duration: Duration(milliseconds: 200), curve: Curves.easeIn);
-        return;
-      }
-      // Switch to Disabled Button.
-      _pageController.animateToPage(0, duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+      _pageController.animateToPage(_getIndexResult(), duration: Duration(milliseconds: 400), curve: Curves.easeIn);
       return;
     });
+  }
+
+  /// This function simply give us the correct button to display + correct message.
+  int _getIndexResult() {
+    // Get persons from search ahead.
+    final providerValue = ref.read(searchPersonsProvider);
+    // An error occured while fetching data.
+    if (providerValue.state == ProviderState.failure) {
+      return 4;
+    }
+    // Switch to ListView Screen Button.
+    if (providerValue.value.isNotEmpty) {
+      return 2;
+    }
+    // Switch to CreateNew Screen Button.
+    if (_firstname.isNotEmpty && _lastname.isNotEmpty && _zonename.isNotEmpty && _jobname.isNotEmpty && providerValue.value.isEmpty) {
+      return 3;
+    }
+    // Switch to Disabled Button.
+    if (_firstname.isNotEmpty || _lastname.isNotEmpty || _zonename.isNotEmpty || _jobname.isNotEmpty) {
+      return 1;
+    }
+    // Default stance.
+    return 0;
   }
 }
